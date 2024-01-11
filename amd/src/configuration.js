afdf85d66154edea67e4c581093bd28af131a145
Thomas Lüdecke <thomas.luedecke@tu-darmstadt.de>
2024-01-11 12:49:06 +0100

    lint

--- amd/src/configuration.js
+++ amd/src/configuration.js
@@ -42,7 +42,7 @@ export const configure = (instanceConfig) => {
                     blobInfo.blob(),
                     blobInfo.filename(),
                     progress
-                ))
+                ));
             }
             return;
         }),
